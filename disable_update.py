677385707f7328e40a07ce325954c561e5659cb9
bigbenzh <85002534+bigbenzh@users.noreply.github.com>
2022-03-17 18:46:47 +0800

    fix patch script

--- disable_update.py
+++ disable_update.py
@@ -1,4 +1,4 @@
-with open("trilium/src/public/app/widgets/buttons/global_menu.js","r+") as f:
+with open("src/public/app/widgets/buttons/global_menu.js","r+") as f:
     d = f.read()
     d = d.replace("const latestVersion = await this.fetchLatestVersion();","const latestVersion = glob.triliumVersion;")
     f.seek(0)
