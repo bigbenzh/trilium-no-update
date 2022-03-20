61bd5157b3bafbe962f2969bbf775184ba55d050
bigbenzh <85002534+bigbenzh@users.noreply.github.com>
2022-03-20 12:08:13 +0800

    Update disable_update.py

--- disable_update.py
+++ disable_update.py
@@ -3,4 +3,5 @@ with open("src/public/app/widgets/buttons/global_menu.js","r+") as f:
     d = d.replace("const latestVersion = await this.fetchLatestVersion();","const latestVersion = glob.triliumVersion;")
     f.seek(0)
     f.write(d)
+    f.truncate() # resize the file to avoid extra bytes
 
